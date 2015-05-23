1c980bad3b9fe95cc54b8bfa4ae3d4c11b1407f2
Lutz Hankewitz <lutz.hankewitz@hankewitz.de>
2015-05-23 23:47:57 +0200

    sum multiple integers

--- src/test/java/AddNumbersTest.java
+++ src/test/java/AddNumbersTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -25,15 +28,31 @@ public class AddNumbersTest {
     @Test
     public void addNumbers_withGiven2Numbers_returnsSum() {
         assertThat(add("1,2"), is(3));
+        assertThat(add("2,3"), is(5));
     }
 
-    private int add(final String numbers) {
-        if(numbers.matches("\\d+")){
-            return Integer.valueOf(numbers);
-        } else if("1,2".equals(numbers)){
-            return 3;
+    private int add(final String numbersAsString) {
+        if(numbersAsString.matches("\\d+")){
+            return Integer.valueOf(numbersAsString);
+        } else if(numbersAsString.matches("\\d+,\\d+")){
+            final String[] singleNumbers = numbersAsString.split(",");
+            final List<Integer> numbers = getAsIntegers(singleNumbers);
+            Integer sum = 0;
+            for (Integer number : numbers) {
+                sum += number;
+            }
+            return sum;
         } else{
             return 0;
         }
+    }
+
+    private List<Integer> getAsIntegers(final String[] singleNumbers) {
+        List<Integer> numbers = new ArrayList<Integer>();
+        for (String singleNumber : singleNumbers) {
+            numbers.add(Integer.valueOf(singleNumber));
+        }
+
+        return numbers;
     }
 }
